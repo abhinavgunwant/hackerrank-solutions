f0e66c7d1a4782cb5a6b7ccdf0c002d6dd0b760b
Abhinav Gunwant <abhigunwant@gmail.com>
2019-06-09 05:02:35 +0530

    Basic project structuring... - Added a folder related to "Problem" - containing the problem statement and the sample test cases from hackerrank hope that adding these to a single place helps :)

--- Tutorials/10 Days of Statistics/Day 0 - Weighted Mean/solution.py
+++ Tutorials/10 Days of Statistics/Day 0 - Weighted Mean/solution.py
@@ -1,13 +1,20 @@
+
+# function to multiply corresponding elements of two arrays
 def multiply(A, B):
     res = []
     for i in range(len(A)):
         res.append(A[i] * B[i])    
     return res
 
-
 n = int(input())
 
-A = list(map(int, input().split()))
+# get all the elements for array X
+X = list(map(int, input().split()))
+
+# get all the weights corresponding to each element in X
 W = list(map(int, input().split()))
 
-print(round(sum(multiply(A,W))/sum(W), 1))
+# calculate the weighted mean
+Mw = round( sum( multiply(X,W) ) / sum(W), 1 )
+
+print(Mw)
