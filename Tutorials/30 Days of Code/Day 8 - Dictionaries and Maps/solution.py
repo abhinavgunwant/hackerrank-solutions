faaba12b7334bb610a965b22111e9a412f8481d2
Abhinav Gunwant <abhigunwant@gmail.com>
2019-06-17 23:16:52 +0530

    Committing Day 8 and Day 9 of 10 days of code...

---
+++ Tutorials/30 Days of Code/Day 8 - Dictionaries and Maps/solution.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import os
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    
+    return n * factorial(n-1)
+
+if __name__ == '__main__':
+    n = int(input())
+    print(factorial(n))
